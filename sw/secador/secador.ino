091bb7d963cbe02258fd8c562c8af931c93f2fcb
smarley2 <fabiocposser@gmail.com>
2016-07-27 17:31:22 -0300

    Adicionado inicializações, timer, leitura ad e verificação de temperatura.

--- sw/secador/secador.ino
+++ sw/secador/secador.ino
@@ -1,9 +1,8 @@
-
 /*
- * Valores padrão para temperatura
- * Temp_max = 120;
- * Temp_min = 110;
- * Temp_alarme = 125;
+ * Código para leitura e controle da temperatura do secador.
+ * Utiliza um sensor LM35 para medir a temperatura.
+ * O controle da temperatura se dá através de uma abertura de ar frio, acionada por um motor DC.
+ * O valor da temperatura é mostrado em um display de 7 segmentos.
  */
 
 
@@ -11,49 +10,188 @@
 // Bibliotecas utilizadas no projeto
 #include <LedControl.h>
 #include <EEPROM.h>
+#include <TimerOne.h>
 
-// Configurar conforme os pinos do hardware obedecendo a ordem abaixo.
-// inputs: DIN pin, CLK pin, LOAD pin. number of chips
-LedControl mydisplay = LedControl(45, 44, 43, 1);
+// Variáveis utilizadas no programa
+byte temp_max = 120, temp_min = 110, temp_alarme = 125; //Valores default para primeira inicialização.
+byte segundo = 0, temperatura = 0;
+int i = 0;
 
-byte Temp_max = 120, Temp_min = 110, Temp_alarme = 125; //Valores default para primeira inicialização.
+// Rele para abrir, fechar e sirene;
+// Botão para menu, decrementar e incrementar;
+int rele_abre = 3, rele_fecha = 5, rele_sirene = 7, input_menu = 8, input_dec = 9, input_inc = 10;
+int fim_curso_abre = 4, fim_curso_fecha = 6, janela_fechada = 0, janela_aberta = 0;
 
 void setup() 
 {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600); // testa com 9600 mas depois aumenta para o máximo que der, para o uC não perder tempo escrevendo
+  Serial.print("Iniciando a configuração\n");
+  inicializa_timer1();
+  Serial.print("Inicialização Timer1\n");
+  inicializa_gpio();
+  Serial.print("Inicialização GPIO\n");
   inicializa_eeprom();
+  Serial.print("Inicialização EEPROM\n");
   inicializa_display();
+  Serial.print("Inicialização Display\n");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  verifica_menu();
 
+  // Roda a rotina da temperatura somente a cada segundo, a dinâmica da temperatura é lenta e não necessita taxa maior.
+  if(segundo == 1)
+  {
+    segundo = 0;
+    
+    // VOUT = 1500 mV at 150°C
+    // VOUT = 250 mV at 25°C
+    // VOUT = –550 mV at –55°C
+    // Fundo de escala em 5V = 500°C = 1023;
+    // Então para passar para °C a medição, multiplica por 500 e depois divide por 1023
+    temperatura = (analogRead(0)*500) / 1023; // Lê o canal 0
+    Serial.print("Temperatura = ");
+    Serial.print(temperatura);
+    Serial.print("\n");
+
+    // Tempertaura menor que a mínima fecha a janela
+    if(temperatura < temp_min)
+    {
+      if(janela_fechada == 0) // flag para indicar que a janela já está fechada e não precisa entrar aqui.
+      {
+        janela_fechada = 1;
+        Serial.print("Fechando Ar\n"); 
+        digitalWrite(rele_abre, LOW); // Desliga o outro rele para garantir que não vai dar curto na fonte.
+        delay(300); // Aguarda 300ms para garantir que o rele está desligado.
+        digitalWrite(rele_fecha, HIGH);
+        while(digitalRead(fim_curso_fecha)){} //Fica preso aqui até que o input se torne 0.
+        digitalWrite(rele_fecha, LOW);
+        Serial.print("Janela fechada\n"); 
+      }
+    }
+
+    // Tempertaura maior que a máxima abre a janela
+    if(temperatura > temp_max)
+    {
+      if(janela_aberta == 0) // flag para indicar que a janela já está aberta e não precisa entrar aqui.
+      {
+        janela_aberta = 1;
+        Serial.print("Abrindo Ar\n"); 
+        digitalWrite(rele_fecha, LOW); // Desliga o outro rele para garantir que não vai dar curto na fonte.
+        delay(300); // Aguarda 300ms para garantir que o rele está desligado.
+        digitalWrite(rele_abre, HIGH);
+        while(digitalRead(fim_curso_fecha)){} //Fica preso aqui até que o input se torne 0.
+        digitalWrite(rele_abre, LOW);
+        Serial.print("Janela aberta\n"); 
+      }
+    }
+
+
+    
+  }
+  
+}
+
+/// --------------------------
+/// Interrupção em 200ms
+/// --------------------------
+void timerIsr()
+{
+  ///// Conta 1 segundo. //////
+    i++; // incrementa i a cada 200ms;
+    if(i == 5)
+    {
+      segundo = 1;
+      i = 0;
+    }
+  /////////////////////////////////////
+    
+}
+
+
+
+/// --------------------------
+/// Menu para seleção dos limites de temperatura
+/// --------------------------
+void verifica_menu()
+{
+    
 }
 
 
 
 
+/// --------------------------
+/// Inicialização do timer
+/// --------------------------
+void inicializa_timer1()
+{
+  Timer1.initialize(200000); // set a timer of length 200000 microseconds (or 0.2 sec - or 5Hz)
+  Timer1.attachInterrupt( timerIsr ); // attach the service routine here
+}
+
+
+/// --------------------------
+/// Inicialização dos pinos digitais
+/// --------------------------
 
 
 
+void inicializa_gpio() // Verificar os pinos escolhidos
+{  
+  pinMode(rele_abre, OUTPUT);
+  digitalWrite(rele_abre, LOW);
+  pinMode(rele_fecha, OUTPUT);
+  digitalWrite(rele_fecha, LOW);
+  pinMode(rele_sirene, OUTPUT);
+  digitalWrite(rele_sirene, LOW); 
+  
+  pinMode(fim_curso_abre, INPUT);
+  pinMode(fim_curso_fecha, INPUT);
+  pinMode(input_menu, INPUT);
+  pinMode(input_dec, INPUT);
+  pinMode(input_inc, INPUT);
+}
 
-
+/// --------------------------
+/// Inicialização da EEPROM
+/// --------------------------
 void inicializa_eeprom()
 {  
 // Inicializa os limites de temperatura para o primeiro uso
 if(EEPROM.read(0) == 5){ // Se está escrito 5 no endereço 0 é porque o uC já foi inicializado, então lê os valores da memória.
-  Temp_max = EEPROM.read(1);
-  Temp_min = EEPROM.read(2);
-  Temp_alarme = EEPROM.read(3);  
+  temp_max = EEPROM.read(1);
+  temp_min = EEPROM.read(2);
+  temp_alarme = EEPROM.read(3);  
   }
   else // Senão inicializa com os seguintes valores
   {
   EEPROM.write(0,5); // Valor para identificar que o uC já foi inicializado antes.
-  EEPROM.write(1,Temp_max);
-  EEPROM.write(2,Temp_min);
-  EEPROM.write(3,Temp_alarme);
+  EEPROM.write(1,temp_max);
+  EEPROM.write(2,temp_min);
+  EEPROM.write(3,temp_alarme);
   }
-
+  
+  Serial.print("temp_max = ");
+  Serial.print(temp_max);
+  Serial.print("\n");
+  Serial.print("temp_min = ");
+  Serial.print(temp_min);
+  Serial.print("\n");
+  Serial.print("temp_alarme = ");
+  Serial.print(temp_alarme);
+  Serial.print("\n");
 }
+
+/// --------------------------
+/// Inicialização do display
+/// --------------------------
+
+// Configurar conforme os pinos do hardware obedecendo a ordem abaixo.
+// inputs: DIN pin, CLK pin, LOAD pin. number of chips
+LedControl mydisplay = LedControl(2, 1, 0, 1);
 
 void inicializa_display()
 {
@@ -72,24 +210,3 @@ void inicializa_display()
 //  mydisplay.clearDisplay(0); // Deve fazer a mesma coisa que o escrito acima.
 ///////////////////////////////////
 }
-
-/* Como utilizar a biblioteca do LedControl com MAX7219
-#include <LedControl.h>
-
-// inputs: DIN pin, CLK pin, LOAD pin. number of chips
-LedControl mydisplay = LedControl(45, 44, 43, 1);
-
-void setup() {
-  mydisplay.shutdown(0, false);  // turns on display
-  mydisplay.setIntensity(0, 15); // 15 = brightest
-  mydisplay.setDigit(0, 0, 9, false);
-  mydisplay.setDigit(0, 1, 8, false);
-  mydisplay.setDigit(0, 2, 7, false);
-  mydisplay.setDigit(0, 3, 6, false);
-  mydisplay.setDigit(0, 4, 5, true);
-  mydisplay.setDigit(0, 5, 4, false);
-  mydisplay.setDigit(0, 6, 3, false);
-  mydisplay.setDigit(0, 7, 2, false);
-}
-*/
-
